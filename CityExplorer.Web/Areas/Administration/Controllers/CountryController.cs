16a700d4dfeb3e597af73c0bd061aa72603baa0d
nikolay-radkov <nikolay.kirilov.radkov@gmail.com>
2014-11-17 00:02:30 +0200

    Add kendo grid for countries management

---
+++ CityExplorer.Web/Areas/Administration/Controllers/CountryController.cs
@@ -0,0 +1,83 @@
+﻿namespace CityExplorer.Web.Areas.Administration.Controllers
+{
+    using AutoMapper;
+    using CityExplorer.Data;
+    using CityExplorer.Models;
+    using CityExplorer.Web.Areas.Administration.Controllers.Base;
+    using CityExplorer.Web.Areas.Administration.ViewModels;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using System;
+    using System.Web.Mvc;
+
+    public class CountryController : AdminController
+    {
+        public CountryController(ICityExplorerData data)
+            : base(data)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult ReadCountries([DataSourceRequest]DataSourceRequest request)
+        {
+            var countriesDataSource = this.Data.Countries.All()
+                .ToDataSourceResult(request, country => Mapper.Map<CountryViewModel>(country));
+
+            return this.Json(countriesDataSource);
+        }
+
+
+        [HttpPost]
+        public ActionResult CreateCountry([DataSourceRequest]DataSourceRequest request, CountryViewModel model)
+        {
+            if (model != null)
+            {
+                var country = new Country();
+                Mapper.Map(model, country);
+
+                country.CreatedOn = DateTime.Now.ToLocalTime();
+                this.Data.Countries.Add(country);
+
+                this.Data.SaveChanges();
+            }
+
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult UpdateCountry([DataSourceRequest]DataSourceRequest request, CountryViewModel model)
+        {
+            if (model != null)
+            {
+                var Country = this.Data.Countries.GetById(model.Id);
+                Mapper.Map(model, Country);
+                Country.ModifiedOn = DateTime.Now.ToLocalTime();
+                this.Data.SaveChanges();
+            }
+
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
+        }
+
+        [HttpPost]
+        public ActionResult DestroyCountry([DataSourceRequest]DataSourceRequest request, CountryViewModel model)
+        {
+            if (model != null)
+            {
+                var Country = this.Data.Countries.GetById(model.Id);
+                Mapper.Map(model, Country);
+                Country.IsDeleted = true;
+                Country.DeletedOn = DateTime.Now.ToLocalTime();
+
+                this.Data.SaveChanges();
+            }
+
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
+        }
+    }
+}
---
+++ CityExplorer.Web/Areas/Administration/ViewModels/CountryViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace CityExplorer.Web.Areas.Administration.ViewModels
+{
+    using CityExplorer.Models;
+    using CityExplorer.Web.Areas.Administration.ViewModels.Base;
+    using CityExplorer.Web.Infrastructure.Mapping;
+    using System;
+    using System.ComponentModel.DataAnnotations;
+    using System.Web;
+    using System.Web.Mvc;
+
+    public class CountryViewModel : IMapFrom<Country>, IHaveCustomMappings
+    {
+        [HiddenInput(DisplayValue = false)]
+        public int? Id { get; set; }
+
+        [Required]
+        [StringLength(100, MinimumLength = 2)]
+        public string Name { get; set; }
+
+        public string Image { get; set; }
+
+        public string  ContinentName { get; set; }
+
+        public int ContinentId { get; set; }
+
+        public void CreateMappings(AutoMapper.IConfiguration configuration)
+        {
+            configuration.CreateMap<Country, CountryViewModel>()
+               .ForMember(c => c.ContinentName, opt => opt.MapFrom(c => c.Continent.Name))
+               .ReverseMap();
+        }
+    }
+}
