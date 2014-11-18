cd313c1d7788293c2a2d02a58103635e1a1c2215
nikolay-radkov <nikolay.kirilov.radkov@gmail.com>
2014-11-18 06:35:42 +0200

    Untracked files

---
+++ CityExplorer.Web/Areas/Administration/Controllers/FeedbackController.cs
@@ -0,0 +1,33 @@
+﻿namespace CityExplorer.Web.Areas.Administration.Controllers
+{
+    using AutoMapper;
+    using CityExplorer.Data;
+    using CityExplorer.Web.Areas.Administration.Controllers.Base;
+    using CityExplorer.Web.Areas.Administration.ViewModels;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using System.Web.Mvc;
+
+    public class FeedbackController : AdminController
+    {
+        public FeedbackController(ICityExplorerData data)
+            : base(data)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult ReadFeedbacks([DataSourceRequest]DataSourceRequest request)
+        {
+            var feedbacksDataSource = this.Data.Feedbacks.All()
+                .ToDataSourceResult(request, feedback => Mapper.Map<FeedbackViewModel>(feedback));
+
+            return this.Json(feedbacksDataSource);
+        }
+    }
+}
