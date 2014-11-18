b77d7ee5e5649439dc72a2b8081b35fc198e4639
nikolay-radkov <nikolay.kirilov.radkov@gmail.com>
2014-11-18 17:09:09 +0200

    Clean solution

---
+++ CityExplorer.Web/Areas/Administration/Controllers/CityController.cs
@@ -0,0 +1,51 @@
+﻿namespace CityExplorer.Web.Areas.Administration.Controllers
+{
+    using AutoMapper;
+    using CityExplorer.Data;
+    using CityExplorer.Web.Areas.Administration.Controllers.Base;
+    using CityExplorer.Web.Areas.Administration.ViewModels;
+    using Kendo.Mvc.Extensions;
+    using Kendo.Mvc.UI;
+    using System;
+    using System.Web.Mvc;
+
+    [Authorize(Roles = "Admin")]
+    public class CityController : AdminController
+    {
+        public CityController(ICityExplorerData data)
+            : base(data)
+        {
+
+        }
+
+        public ActionResult Index()
+        {
+            return this.View();
+        }
+
+        [HttpPost]
+        public ActionResult ReadCities([DataSourceRequest]DataSourceRequest request)
+        {
+            var citiesDataSource = this.Data.Cities.All()
+                .ToDataSourceResult(request, cities => Mapper.Map<CityViewModel>(cities));
+
+            return this.Json(citiesDataSource);
+        }
+
+        [HttpPost]
+        public ActionResult DestroyCity([DataSourceRequest]DataSourceRequest request, CityViewModel model)
+        {
+            if (model != null)
+            {
+                var city = this.Data.Cities.GetById(model.Id);
+                Mapper.Map(model, city);
+                city.IsDeleted = true;
+                city.DeletedOn = DateTime.Now.ToLocalTime();
+
+                this.Data.SaveChanges();
+            }
+
+            return this.Json(new[] { model }.ToDataSourceResult(request, this.ModelState));
+        }
+    }
+}
---
+++ CityExplorer.Web/Areas/Administration/ViewModels/CityViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace CityExplorer.Web.Areas.Administration.ViewModels
+{
+    using CityExplorer.Models;
+    using CityExplorer.Web.Infrastructure.Mapping;
+    using System.ComponentModel.DataAnnotations;
+
+    public class CityViewModel : IMapFrom<City>
+    {
+        public int? Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        public double Area { get; set; }
+
+        public long Population { get; set; }
+
+        public string Website { get; set; }
+
+        public string SettledYear { get; set; }
+
+        public string Description { get; set; }
+    }
+}
